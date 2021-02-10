844224b144e5ba5d15358d68de144e056a7ae31d
prpabba <prpabba@JRHNFB3.corp.copart.com>
2021-02-11 00:00:15 +0530

    first comit

---
+++ helloworld/src/helloworld/pabba.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class pabba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Preetham");
+		for(int i=0;i<5;i++) {
+			System.out.println("_");
+		}
+	}
+
+}
